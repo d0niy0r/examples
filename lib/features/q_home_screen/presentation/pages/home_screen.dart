6fcd631947f35624cb4821e90f8ed96b3f8b3413
AzizbekAsqaraliyev <DasturchiMobile@gmail.com>
2024-03-03 23:55:53 +0500

    lesson 5

--- lib/features/q_home_screen/presentation/pages/home_screen.dart
+++ lib/features/q_home_screen/presentation/pages/home_screen.dart
@@ -2,9 +2,7 @@ import 'package:example/core/styles/colors.dart';
 import 'package:example/features/q_home_screen/presentation/widgets/q_home_appbar.dart';
 import 'package:example/features/q_home_screen/presentation/widgets/question_rating.dart';
 import 'package:example/features/qiuz/presentation/pages/home_screen.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -26,7 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           children: [
             const SizedBox(height: 50),
-            QuestionRating(),
+            const QuestionRating(),
             const SizedBox(height: 20),
             Expanded(
               child: ListWheelScrollView(
@@ -54,11 +52,11 @@ class _HomeScreenState extends State<HomeScreen> {
                             onTap: () {
                               Navigator.of(context).push(MaterialPageRoute(
                                   builder: (context) =>
-                                      QuizScreen(title: "Art and Literature")));
+                                      const QuizScreen(title: "Art and Literature")));
                             },
                             leading:
-                                Icon(Icons.draw, size: 30, color: Colors.white),
-                            title: Text("Art and Literature",
+                                const Icon(Icons.draw, size: 30, color: Colors.white),
+                            title: const Text("Art and Literature",
                                 style: TextStyle(
                                     color: Colors.white, fontSize: 20)),
                           ),
